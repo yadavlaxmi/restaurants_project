3729cc796dc826bef49eca39013fb38217c294e4
Laxmi <laxmiyadav21@navgurukul.org>
2023-11-02 15:07:59 +0530

    first commit for restaurants project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Layout from "./Componenet/Layout";
+function App() {
+  return (
+    <div className="App">
+      <h1>Resturant Website</h1>
+      <Layout/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componenet/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+const Layout = () => {
+  return (
+    <>
+    <div>
+        <Header/>
+        <h1> Layout</h1>
+    </div>
+    </>
+  )
+}
+
+export default Layout
