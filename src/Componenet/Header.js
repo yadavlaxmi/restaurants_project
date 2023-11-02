585dc6b4ebeba1ad70b1c73eb78a685135a08856
Laxmi <laxmiyadav21@navgurukul.org>
2023-11-02 17:19:49 +0530

    create header for restarurant page

--- src/Componenet/Header.js
+++ src/Componenet/Header.js
@@ -1,12 +1,36 @@
 import React from 'react'
-import { AppBar,Box,Typography } from '@mui/material'
+import { AppBar,Box,Toolbar,Typography } from '@mui/material'
+import FastfoodTwoToneIcon from '@mui/icons-material/FastfoodTwoTone';
+import {Link} from '@mui/material';
 const Header = () => {
   return (
     <>
     <Box>
-        <AppBar>
-            <Typography>My Restaurant</Typography>
-        </AppBar>
+        <AppBar component={"nav"} sx={{bgcolor:"black"}}>
+            <Toolbar>
+            <Typography color={"goldenrod"} variant='h6' component="div" sx={{flexGrow:1}}>
+                <FastfoodTwoToneIcon/>
+                My Restaurant
+                </Typography>
+        
+            <Box>
+                <ul>
+                    <li>
+                        <Link to={"/"}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to={"/about"}>About</Link>
+                    </li>
+                    <li>
+                        <Link to={"/contact"}>Contact</Link>
+                    </li>
+                    <li>
+                        <Link to={"/menu"}>Menu</Link>
+                    </li>
+                </ul>
+            </Box>
+            </Toolbar>
+            </AppBar>
     </Box>
     </>
   )
