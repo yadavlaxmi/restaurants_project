ec29521359be036ef5a725095c7b8fca18ecdcfa
Laxmi <laxmiyadav21@navgurukul.org>
2023-11-02 20:30:51 +0530

    for adding icon

--- src/Componenet/Header.js
+++ src/Componenet/Header.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import { AppBar,Box,Toolbar,Typography } from '@mui/material'
+import React,{useState} from 'react'
+import { AppBar,Box,IconButton,Toolbar,Typography } from '@mui/material'
 import FastfoodTwoToneIcon from '@mui/icons-material/FastfoodTwoTone';
 import {Link} from '@mui/material';
 import "../../src/Styles/HeaderStyle.css";
+import MenuIcon from '@mui/icons-material/Menu';
 const Header = () => {
+    
   return (
     <>
     <Box>
         <AppBar component={"nav"} sx={{bgcolor:"black"}}>
             <Toolbar>
+                <IconButton color="inherit" aria-label= "open drawer" edge="start" sx={{mr:2, display:{xs:"none",sm:"block"}}}>
+                    <MenuIcon/>
+                </IconButton>
             <Typography color={"goldenrod"} variant='h6' component="div" sx={{flexGrow:1}}>
                 <FastfoodTwoToneIcon/>
                 My Restaurant
